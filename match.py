43932ca74d424075e1e435966c52626dc31b5c51
Samuel William Skillman <skillman@bullet0001.slac.stanford.edu>
2014-10-10 18:32:56 -0700

    Modifying match.py to query photo catalog, grab the half light radius.

--- match.py
+++ match.py
@@ -33,9 +33,15 @@ def SDSS_select(sql):
 # We want a sample of galaxies and their 5-band photometry, plus their
 # redshifts and possibly positions.
 
-galaxies = "SELECT top 10000 objid, ra, dec, z, dered_u AS mag_u,\
-    dered_g AS mag_g, dered_r AS mag_r, dered_i AS mag_i,\
-    dered_z AS mag_z FROM SpecPhoto WHERE (class = 'Galaxy')"
+ngal = 500000
+galaxies = "SELECT top %i objid, ra, dec, dered_u, dered_g, \
+dered_r, dered_i, dered_z, expRad_r FROM PhotoObj as p WHERE (type = 3) \
+AND p.dered_i BETWEEN 0.0 AND 22.0"  % (ngal)
+
+#galaxies = "SELECT top 500000 objid, ra, dec, z, dered_u AS mag_u,\
+#dered_g AS mag_g, dered_r AS mag_r, dered_i AS mag_i,\
+#dered_z AS mag_z FROM SpecPhoto as p WHERE (class = 'Galaxy')\
+#AND (p.z BETWEEN 0.1 AND 0.2)"
 print galaxies
 
 # This SQL checks out at the [SkyServer SQL
@@ -45,31 +51,44 @@ gal_file = SDSS_select(galaxies)
 mytype = np.dtype([('id', 'int64'),
                    ('ra', 'float32'),
                    ('dec', 'float32'),
-                   ('redshift', 'float32'),
                    ('u', 'float32'),
                    ('g', 'float32'),
                    ('r', 'float32'),
                    ('i', 'float32'),
                    ('z', 'float32'),
+                   ('radius', 'float32'),
                    ])
 data = np.loadtxt(gal_file, dtype=mytype, delimiter=',', skiprows=2)
-data.sort(order='r')
+data.sort(order='i')
 data = data[::-1]
+print 'Originally pulled down %i halos' % data.size
+
+mask = \
+        (data['g'] < 22) * \
+        (data['r'] < 22) * \
+        (data['i'] < 22)
+        #(data['u'] < 22) * \
+        #* \
+        #(data['z'] < 22)
+cleaned = data[mask]
+
+print 'Left with %i halos' % cleaned.size
 
 header = """# SDF 1.0
 parameter byteorder = 0x78563412;
-#SDSS Galaxies
-#%s
-struct{
+# SDSS Galaxies
+# %s
+struct {
     int64_t id;
-    float ra, dec, redshift;
+    float ra, dec;
     float u, g, r, i, z;
-};
+    float radius;
+}[%i];
 #\x0c
 # SDF-EOH
-""" % galaxies
+""" % (galaxies, cleaned.size)
 
 f = file("galaxies.sdf", "wb")
 f.write(header)
-data.tofile(f)
+cleaned.tofile(f)
 f.close()
